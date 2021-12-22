31d05ff5219ab2da865bccfbbe3f46ba392feb6e
Russ Taylor <russ.taylor812@gmail.com>
2021-12-22 15:47:59 -0800

    commit

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Russell Taylor',
+    site_url: 'https://russwtaylor.com',
+    feed_url: 'https://russwtaylor.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
